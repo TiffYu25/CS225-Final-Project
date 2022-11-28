89028a519962aabfba756b79a5aa212ad18e4cd0
eloutf <117325404+eloutf@users.noreply.github.com>
2022-11-28 17:25:34 -0600

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,14 +9,13 @@
 
 TEST_CASE("file reads properly check 1", "[test]") {
     string filename = "../tests/example 1.txt";
-    wiki test(filename);
+    wiki test(filename, 5);
     vector<vector<int>> example = test.getAdj();
-    const vector<vector<int>> correct_example = {
-        {1, 2, 3, 4},\
-        {4, 3, 2, 0}, \
-        {0, 3, 4, 1}, \
-        {1, 2, 4, 0}, \
-        {2, 3, 0, 1}
+    const vector<vector<int>> correct_example = {{1, 2, 3, 4},\
+     {4, 3, 2, 0}, \
+     {0, 3, 4, 1}, \
+     {1, 2, 4, 0}, \
+     {2, 3, 0, 1}
      };
 
     REQUIRE(example == correct_example);
@@ -24,14 +23,13 @@ TEST_CASE("file reads properly check 1", "[test]") {
 
 TEST_CASE("file reads properly check 2", "[test]") {
     string filename = "../tests/example 2.txt";
-    wiki test(filename);
+    wiki test(filename, 5);
     vector<vector<int>> example = test.getAdj();
-    const vector<vector<int>> correct_example = {
-        {39, 16, 27, 15},\
-        {5, 8}, \
-        {13, 3, 4}, \
-        {18, 39, 80, 1128474}, \
-        {3879252, 93, 76, 15, 18}
+    const vector<vector<int>> correct_example = {{39, 16, 27, 15},\
+     {5, 8}, \
+     {13, 3, 4}, \
+     {18, 39, 80, 1128474}, \
+     {3879252, 93, 76, 15, 18}
      };
 
     REQUIRE(example == correct_example);
@@ -39,32 +37,30 @@ TEST_CASE("file reads properly check 2", "[test]") {
 
 TEST_CASE("file reads properly check 3", "[test]") {
     string filename = "../tests/example 3.txt";
-    wiki test(filename);
+    wiki test(filename, 6);
     vector<vector<int>> example = test.getAdj();
     const vector<vector<int>> correct_example = {
         {39, 16, 27, 15},\
         {5, 8}, \
         {13, 3, 4}, \
         {18, 39, 80, 1128474}, \
-        {3879252, 93, 7689, 15, 18}
+        {3879252, 93, 7689, 15, 18},
         {1412456, 69420, 420, 567, 5, 678, 40}
      };
 
     REQUIRE(example == correct_example);
 }
 
-
 TEST_CASE("DFS check 1", "[test]") {
     //filename doesn't matter here since vector will be passed into adj setter, but I assume calling default constructor
     //and running on wiki txt will just make it rilly slow
     string filename = "../tests/example 1.txt";
-    wiki test(filename);
-    const vector<vector<int>> test_graph = {
-        {3, 2, 1},\
-        {2, 0}, \
-        {4, 1, 0}, \
-        {0}, \
-        {2}
+    wiki test(filename, 5);
+    const vector<vector<int>> test_graph = {{3, 2, 1},\
+     {2, 0}, \
+     {4, 1, 0}, \
+     {0}, \
+     {2}
      };
     test.setAdj(test_graph);
     test.DFS(0, 5);
@@ -75,7 +71,7 @@ TEST_CASE("DFS check 1", "[test]") {
 
 TEST_CASE("DFS check 2", "[test]") {
     string filename = "../tests/example 2.txt";
-    wiki test(filename);
+    wiki test(filename, 5);
     const vector<vector<int>> test_graph = {
         {1, 4, 3},\
         {0, 4}, \
