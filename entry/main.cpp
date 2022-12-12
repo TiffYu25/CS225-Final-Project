597487dc76d8f161b988a26cf4705a5f4ba49023
Tiffany Yu <tyu19@illinois.edu>
2022-12-11 21:34:19 -0600

    Update main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -9,29 +9,29 @@ int main() {
     // vector<vector<int>> adjacency = test.getAdj();
 
     const vector<vector<int>> adj = {
-        {1},\
-        {2},\
-        {4, 3},\
+        {2, 3},\
         {0},\
-        {5},\
-        {6},\
-        {7},\
+        {1},\
+        {4},\
         {}
     };
 
-    int num = 8;
+    int num = 5;
     wiki g(adj, num);
-    // Create Reverse Graph
-    wiki gr = g.reverse(num);
+    // // Create Reverse Graph
+    // wiki gr = g.reverse(num);
 
-    g.SCC(num);
-    vector<vector<int>> scc = g.getSCC();
-    for (vector<int> v : scc) {
-        for (int i : v) {
-            cout << i;
-        }
-        cout << endl;
-    }
+    // SCC
+    // g.SCC(num);
+    // vector<vector<int>> scc = g.getSCC();
+    // for (vector<int> v : scc) {
+    //     for (int i : v) {
+    //         cout << i;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Print Adj List
     // vector<vector<int>> adjacency = gr.getAdj();
     // for (unsigned int i = 0; i < adjacency.size(); i++) {
     //     for (int hyperlink : adjacency[i]) {
