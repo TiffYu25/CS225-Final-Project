670338a3b0f6cdc56df475861600d8a076cdae74
Tiffany Yu <tyu19@illinois.edu>
2022-12-11 21:25:47 -0600

    Created SCC Method

--- src/wiki.cpp
+++ src/wiki.cpp
@@ -11,6 +11,7 @@ bool is_digits(const std::string &str) {
 wiki::wiki(vector<vector<int>> adjacency, int num) {
     node_num = num;
     adj = adjacency;
+}
 
 wiki::wiki(string filename, int num) {
     file = filename;
@@ -113,18 +114,17 @@ void wiki::DFS(int root, int num) {
     stack<int> stk;
     stk.push(root);
     visited[root] = true;
+    
     while (!stk.empty()) {
         int curr = stk.top();
         stk.pop();
         traversal.push_back(curr);
-        scc[scc.size() - 1].push_back(curr);
         for (unsigned int i = 0; i < adj[curr].size(); i++) {
             if (!visited[adj[curr][i]]) {
                 stk.push(adj[curr][i]);
                 visited[adj[curr][i]] = true;
             }
         }
-        s_.push(curr);
     }
 }
 
@@ -132,54 +132,47 @@ vector<int> wiki::getTraversal() {
     return traversal;
 }
 
-void wiki::fillOrder(int v, vector<bool> visited, stack<int> &Stack) {
-    // Mark the current node as visited and print it
+void wiki::fillOrder(int v, vector<bool>& visited, stack<int> &Stack) {
     visited[v] = true;
- 
-    // Recur for all the vertices adjacent to this vertex
     for (auto a : adj[v]) {
-        if (!visited[a]) {
-            fillOrder(a, visited, Stack);
-        }
+        if (!visited[a]) { fillOrder(a, visited, Stack); }
     }
-    // All vertices reachable from v are processed by now, push v
     Stack.push(v);
 }
 
 wiki wiki::reverse(int num) {
     wiki w(vector<vector<int>>(num, vector<int>()), num);
     for (int u = 0; u < num; u++) {
-        vector<int>::iterator i;
-        for (int v : adj[u]) {
-            w.adj[v].push_back(u);
-        }
+        for (int v : adj[u]) { w.adj[v].push_back(u); }
     }
     return w;
 }
 
-void wiki::DFS(int s, vector<bool> visitedV) {
+void wiki::DFS(int s, vector<bool>& visitedV, vector<int>& components) {
     visitedV[s] = true;
-    scc[scc.size() - 1].push_back(s);
+    components.push_back(s);
     for (int i : adj[s]) {
-        if (!visitedV[i]) {
-            DFS(i, visitedV);
-        }  
+        if (!visitedV[i]) { DFS(i, visitedV, components); }  
     }
 }
 
 void wiki::SCC(int num) {
+    stack<int> Stack;
+    // Fill vertices in stack according to their finishing times in DFS
+    vector<bool> visited(num, false);
+    for (int i = 0; i < num; i++) {
+        if (!visited[i]) { fillOrder(i, visited, Stack); }
+    }
+    
     vector<bool> visitedV(num, false);
     wiki gr = reverse(num);
-
-    // Create a copy of s_
-    stack<int> Stack = s_;
-
     while (!Stack.empty()) {
         int s = Stack.top();
         Stack.pop();
-        if (visitedV[s] == false) {
-            scc.push_back(vector<int>());
-            gr.DFS(s, visitedV);
+        if (!visitedV[s]) {
+            vector<int> components;
+            gr.DFS(s, visitedV, components);
+            scc.push_back(components);
         }
     }
 }
