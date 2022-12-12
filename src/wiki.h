25d6d8b4e08d21e3b7d4f0f0db2606dbc7793db9
Tiffany Yu <tyu19@illinois.edu>
2022-12-11 21:26:21 -0600

    Created SCC Method

--- src/wiki.h
+++ src/wiki.h
@@ -15,22 +15,22 @@ public:
     int getIndex(string article) { return articleIdx[article]; }
     string getArticle(int idx);
     vector<int> getTraversal();
-    stack<int> getStack() { return s_; }
     vector<vector<int>> getSCC() { return scc; }
     void reader(string filename, int num);
     void DFS(int root, int num);
-    void DFS(int s, vector<bool> visitedV);
 
-    void fillOrder(int v, vector<bool> visited, stack<int> &Stack);
-
+    // DFS method only used for scc
+    void DFS(int s, vector<bool>& visitedV, vector<int>& components);
+    // Creates a Stack specific for SCC
+    void fillOrder(int v, vector<bool>& visited, stack<int> &Stack);
+    // Reverses an adjacency List
     wiki reverse(int num);
-
+    // Computes strongly connected components
     void SCC(int num);
 
 private:
     vector<vector<int>> adj; // adjacency lists for articles
     map<string, int> articleIdx; // Contains the indexes for each article
-    stack<int> s_;
     string file; //contains filename for edges file
     vector<int> traversal; //vector of nodes visited in order of dfs traversal
     vector<vector<int>> scc;
