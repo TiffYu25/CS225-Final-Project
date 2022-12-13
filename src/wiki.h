434b54a0ddfcfb49dd3f721bdbef8ac5fee82ec6
Tiffany Yu <tyu19@illinois.edu>
2022-12-12 19:35:48 -0600

    Added SCC output method

--- src/wiki.h
+++ src/wiki.h
@@ -6,35 +6,46 @@
 
 using namespace std;
 
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+#include <stack>
+
+using namespace std;
+
 class wiki {
 public:
-    wiki(vector<vector<int>> adjacency, int num);
-    wiki(string filename, int num);
-    vector<vector<int>> getAdj();
-    void setAdj(vector<vector<int>> toset);
-    int getIndex(string article) { return articleIdx[article]; }
-    string getArticle(int idx);
-    vector<int> getTraversal();
-    vector<vector<int>> getSCC() { return scc; }
-    void reader(string filename, int num);
-    void DFS(int root, int num);
+    wiki(vector<vector<unsigned int>> adjacency, unsigned int num);
+    wiki(string filename, unsigned int num);
+    vector<vector<unsigned int>> getAdj();
+    void setAdj(vector<vector<unsigned int>> toset);
+    string getArticle(unsigned int idx);
+    map<unsigned int, string> getMap() { return articleMap; }
+    vector<unsigned int> getTraversal();
+    void reader(string filename, unsigned int num);
+    void DFS(unsigned int root, unsigned int num);
+    vector<vector<unsigned int>> getSCC() { return scc; }
 
     // DFS method only used for scc
-    void DFS(int s, vector<bool>& visitedV, vector<int>& components);
+    void DFS(unsigned int s, vector<bool>& visitedV, vector<unsigned int>& components);
     // Creates a Stack specific for SCC
-    void fillOrder(int v, vector<bool>& visited, stack<int> &Stack);
+    void fillOrder(unsigned int v, vector<bool>& visited, stack<unsigned int> &Stack);
     // Reverses an adjacency List
-    wiki reverse(int num);
+    wiki reverse(unsigned int num);
     // Computes strongly connected components
-    void SCC(int num);
+    void SCC(unsigned int num);
 
-    void writeSCCToFile();
+    void fileToArticle();
+    void writeSCCToFile(unsigned int index);
 
 private:
-    vector<vector<int>> adj; // adjacency lists for articles
-    map<string, int> articleIdx; // Contains the indexes for each article
+    vector<vector<unsigned int>> adj; // adjacency lists for articles
+    map<unsigned int, string> articleMap; // Contains the indexes for each article
     string file; //contains filename for edges file
-    vector<int> traversal; //vector of nodes visited in order of dfs traversal
-    vector<vector<int>> scc;
-    int node_num = 4203323; //total number of nodes
+    vector<unsigned int> traversal; //vector of nodes visited in order of dfs traversal
+    vector<vector<unsigned int>> scc;
+    unsigned int node_num = 4206289; //total number of nodes
 };
+
+vector<string> SplitString(const string & str1, char sep);
